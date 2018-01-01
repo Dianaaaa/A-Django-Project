ef2dd8bcec1c9f1f2add5f4ecfa7751457e9e2ce
Diana <1035254583@qq.com>
2018-01-01 14:37:09 +0800

    A Hellow page.

---
+++ HelloWorld/HelloWorld/view.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+#from django.http import HttpResponse
+
+#def hello(request):
+#    return HttpResponse("Hello world ! ")
+
+from django.shortcuts import render
+
+def hello(request):
+#    context          = {}
+#    context['hello'] = 'Hello World!'
+    return render(request, 'base.html')
