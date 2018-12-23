dcfe9f519d25991d43e3cd87baa0e972553641f9
Dianaaaa <1035254583@qq.com>
2018-12-23 10:03:49 +0800

    Add template

--- HelloWorld/HelloWorld/settings.py
+++ HelloWorld/HelloWorld/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'HelloWorld.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+"/templates",],
+        'DIRS': [BASE_DIR+"/templates",],   # 设置template的位置
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
